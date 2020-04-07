beb3ac23c1f57ff076e9542bd43e427515c24393
jayakrishnanrnair <jayakrishnanr.89@gmail.com>
2020-04-07 11:42:38 -0400

    Changed header to functional component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
+const Header = (props) => {
+  return (
+    <div className="header">
         <a href="#default" className="logo">
         <img src="https://www.caesars.com/myrewards/profile/images/caesars_rewards_logo_white.png"></img>  
         </a>
@@ -16,11 +14,11 @@ class Header extends React.Component {
           <a href="#booknow">Book Now</a>
         </div>
         <div className="header-right">
-        <img id="mytr_icon" class="user-icon-img" src="https://www.caesars.com/myrewards/profile/images/user-icon.svg" width="40" height="40" alt="User Icon" />
+        <img id="mytr_icon" className="user-icon-img" src="https://www.caesars.com/myrewards/profile/images/user-icon.svg" width="40" height="40" alt="User Icon" />
           <a className="active" href="#myaccount">My Account</a>
         </div>
       </div>
-    );
-  }
-}
+  );
+};
+
 export default Header;
