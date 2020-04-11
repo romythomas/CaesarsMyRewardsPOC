f1dc5eb6c36cedd0b353aa3e8a2c9db1f0462af5
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-04-10 20:43:15 -0400

    My Offers list view design

--- src/components/MyOffers/OfferItem.js
+++ src/components/MyOffers/OfferItem.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const OfferItem = (props) => { 
+const OfferItem = (props) => {
+    // Box type design
+    /*
     return (
         <div className="item">
 		    <p>{props.offer}</p>
 		</div>
+    );
+    */
+   //List view design
+   return (
+        <div className="listItem">
+            <p>{props.offer}</p>
+        </div>
     );
 }
 
