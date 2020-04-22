b1c9392d183c372a247c3d964da23b4c190fe11d
rkoshy <Rinchu.Koshy.contractor@hilton.com>
2020-04-22 09:15:56 -0500

    Custom Button and Text box added

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,19 @@ module.exports = {
 			test: /\.js$/,
 			exclude: /node_modules/,
 			loader: 'babel-loader'
-		}]
+		},
+		{
+			test: /\.scss$/,
+			  use: [
+				// Creates `style` nodes from JS strings
+				'style-loader',
+				// Translates CSS into CommonJS
+				'css-loader',
+				// Compiles Sass to CSS
+				'sass-loader',
+			  ]
+		}
+	]
 	},
 	plugins:[
 		new HWP(
