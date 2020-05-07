8ade1a333d0ca31f4eddc45d229d5f47ed815f1e
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-07 14:57:47 -0400

    Progressbar design

--- src/components/MyRewards/ProgressBarItem.js
+++ src/components/MyRewards/ProgressBarItem.js
@@ -9,43 +9,48 @@ const mapStateToProps = state => ({
   logininfo: state.guestProfile.logininfo
 });
 
-const mystyle = {
-  backgroundColor: "#ad9692",
-  background: "-webkit-linear-gradient(top, #ad9692, #ad9692)"
-};
-
 /**
  * 
  * @param {*} props 
  */
 const ProgressBarItem = (props) => { 
-    return (        
-        <div className="reward-progress">
+    return (
+      <div className="reward-progress">
+        <div className="progressbar"></div>
         <ul>
-          <li style={mystyle}>
-            <div className="progress-icon"><img src="images/icon-gold.png" alt="Caesars"/></div>
+          <li>
+            <div className="progress-icon">
+              <img src="images/icon-gold.png" alt="Caesars" />
+            </div>
             <div className="progress-text">
+              <samp>0</samp>
+              <strong>5000</strong>
               <span>Gold</span>
             </div>
           </li>
           <li>
-            <div className="progress-icon"><img src="images/icon-platinum.png" alt="Caesars"/></div>
+            <div className="progress-icon">
+              <img src="images/icon-platinum.png" alt="Caesars" />
+            </div>
             <div className="progress-text">
-              <strong>5000</strong>
+              <strong>15000</strong>
               <span>Platinum</span>
             </div>
           </li>
           <li>
-            <div className="progress-icon"><img src="images/icon-diomond.png" alt="Caesars"/></div>
+            <div className="progress-icon">
+              <img src="images/icon-diomond.png" alt="Caesars" />
+            </div>
             <div className="progress-text">
-              <strong>15000</strong>
+              <strong>25000</strong>
               <span>Diomond</span>
             </div>
           </li>
           <li>
-            <div className="progress-icon"><img src="images/icon-gold.png" alt="Caesars"/></div>
+            <div className="progress-icon">
+              <img src="images/icon-gold.png" alt="Caesars" />
+            </div>
             <div className="progress-text">
-              <strong>25000</strong>
               <span>Seven Star</span>
             </div>
           </li>
