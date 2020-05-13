7ca53f81d6c54bf57c0e4759d328f797301223c5
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-12 21:50:03 -0400

    Code optimization in my offers index class file

--- src/components/MyOffers/index.js
+++ src/components/MyOffers/index.js
@@ -37,193 +37,204 @@ class MyOffers extends Component {
     constructor() {
         super();
         this.selectedOfferSort = "";
+        this.selectedOfferFilters = [];
 
-        let selectedOfferFilters = [];
-        let selectedOfferSort = "";
+        //This binding is necessary to make `this` work in the callback
+        this.onLocationChange = this.onLocationChange.bind(this);
+        this.onDateRangeChange = this.onDateRangeChange.bind(this);
+        this.onOfferTypeChange = this.onOfferTypeChange.bind(this);
+        this.onSortingChange = this.onSortingChange.bind(this);
+        this.onOfferCodeChange = this.onOfferCodeChange.bind(this);
+        this.clearFilter = this.clearFilter.bind(this);
 
-        this.updateOfferList = () => {
-            const {offers, markets, getFilteredSortedOffers} = this.props;
-            selectedOfferSort = selectedOfferSort ? selectedOfferSort : this.getDefaultSortValue();
-            this.selectedOfferSort = selectedOfferSort;
-            let filteredSortedOffers = filterOffers(offers, selectedOfferFilters, markets);
-            filteredSortedOffers = sortOffers(filteredSortedOffers, selectedOfferSort);
-            getFilteredSortedOffers(filteredSortedOffers, selectedOfferFilters);
+    }
+
+    updateOfferList() {
+        let {selectedOfferFilters, selectedOfferSort} = this;
+        const {offers, markets, getFilteredSortedOffers} = this.props;
+        this.selectedOfferSort = selectedOfferSort ? selectedOfferSort : this.getDefaultSortValue();
+        let filteredSortedOffers = filterOffers(offers, selectedOfferFilters, markets);
+        filteredSortedOffers = sortOffers(filteredSortedOffers, selectedOfferSort);
+        this.selectedOfferFilters = selectedOfferFilters;
+        getFilteredSortedOffers(filteredSortedOffers, selectedOfferFilters);
+    }
+
+    filterOffers(type, value) {
+        let {selectedOfferFilters} = this;
+        const newFilter = {
+            filterType: type,
+            filterValue: value
+        };
+        this.selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, newFilter);
+        this.updateOfferList();
+    }
+
+    sortOffers(sortType) {
+        if(!sortType) {
+            sortType = this.getDefaultSortValue();
         }
+        this.selectedOfferSort = sortType;
+        this.updateOfferList();
+    }
 
-        this.filterOffers = (type, value) => {
-            const newFilter = {
-                filterType: type,
-                filterValue: value
-            };
-            selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, newFilter);
-            this.updateOfferList();
+    getDefaultSortValue() {
+        const offerSortTypes = getOfferSortTypes();
+        if(offerSortTypes && offerSortTypes.length) {
+            return offerSortTypes[0].value;
         }
+    }
 
-        this.sortOffers = (sortType) => {
-            if(!sortType) {
-                sortType = this.getDefaultSortValue();
+    onLocationChange(value) {
+        const locationValue = value ? value : null;
+        this.filterOffers("location", locationValue);
+    }
+
+    onDateRangeChange(type, startDate, endDate) {
+        if(startDate && endDate) {
+            this.filterOffers(type ? type : "date", {
+                startDate: startDate,
+                endDate: endDate,
+            });
+        }
+    }
+
+    onOfferTypeChange(value) {
+        this.filterOffers("type", (value && value.length) ? value : []);
+    }
+
+    onOfferCodeChange(element) {
+        const offerCode = element ? element : "";
+        this.filterOffers("code", offerCode);
+    }
+
+    onSortingChange(sort) {
+        let sortValue = "";
+        if(sort && sort.target && sort.target.text && sort.target.attributes && sort.target.attributes.length && sort.target.attributes[0].value) {
+            sortValue = sort.target.attributes[0].value;
+        } else {
+            sortValue = this.getDefaultSortValue();
+        }
+        this.sortOffers(sortValue);
+    }
+
+    clearFilter() {
+        this.applyDefaultFilterAndSort(true);
+    }
+
+    createDefaultFilterAndSort(searchParams) {
+        this.selectedOfferFilters = [];
+        let {selectedOfferFilters} = this;
+        let filterStartDate = getMoment();
+        let filterEndDate = getMoment().add(1, 'month');
+        selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
+            filterType: "date",
+            filterValue: {
+                startDate: filterStartDate,
+                endDate: filterEndDate
             }
-            selectedOfferSort = sortType;
-            this.updateOfferList();
-        }
-
-        this.getDefaultSortValue = () => {
-            const offerSortTypes = getOfferSortTypes();
-            if(offerSortTypes && offerSortTypes.length) {
-                return offerSortTypes[0].value;
-            }
-        }
-
-        this.onLocationChange = value => {
-            const locationValue = value ? value : null;
-            this.filterOffers("location", locationValue);
-        }
-
-        this.onDateRangeChange = (type, startDate, endDate) => {
-            if(startDate && endDate) {
-                this.filterOffers(type ? type : "date", {
-                    startDate: startDate,
-                    endDate: endDate,
+        });
+        if(searchParams && Object.entries(searchParams).length) {
+            const {regioncode, propcode, startdate, enddate, flexiblemonth, type, offercode, defaultsort} = searchParams;
+            if(flexiblemonth) {
+                const monthMoment = getMoment().month(flexiblemonth);
+                if(monthMoment.isValid()) {
+                    if(monthMoment.month() === getMoment().month()) {
+                        filterStartDate = getMoment();
+                        filterEndDate = getMoment().endOf('month');
+                    } else if(monthMoment.isBefore(getMoment())) {
+                        filterStartDate = monthMoment.clone().add('years', 1).startOf('month');
+                        filterEndDate = monthMoment.clone().add('years', 1).endOf('month');
+                    } else {
+                        filterStartDate = monthMoment.clone().startOf('month');
+                        filterEndDate = monthMoment.clone().endOf('month');
+                    }
+                }
+                selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
+                    filterType: "month",
+                    filterValue: {
+                        startDate: filterStartDate,
+                        endDate: filterEndDate
+                    }
+                });
+            } else if(startdate || enddate) {
+                const urlStartDate = getMoment(startdate);
+                const urlEndDate = getMoment(enddate);
+                if(urlStartDate.isValid() && urlStartDate.isSameOrAfter(getMoment(), 'day')) {
+                    filterStartDate = urlStartDate;
+                }
+                if(urlEndDate.isValid() && urlEndDate.isSameOrAfter(urlStartDate, 'day')) {
+                    filterEndDate = urlEndDate;
+                } else {
+                    filterEndDate = filterStartDate.clone().add(1, 'month');
+                }
+                selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
+                    filterType: "date",
+                    filterValue: {
+                        startDate: filterStartDate,
+                        endDate: filterEndDate
+                    }
                 });
             }
-        }
-
-        this.onOfferTypeChange = value => {
-            this.filterOffers("type", (value && value.length) ? value : []);
-        }
-
-        this.onOfferCodeChange = element => {
-            const offerCode = element ? element : "";
-            this.filterOffers("code", offerCode);
-        }
-
-        this.onSortingChange = sort => {
-            let sortValue = "";
-            if(sort && sort.target && sort.target.text && sort.target.attributes && sort.target.attributes.length && sort.target.attributes[0].value) {
-                sortValue = sort.target.attributes[0].value;
-            } else {
-                sortValue = this.getDefaultSortValue();
+            if(propcode || regioncode) {
+                const {markets} = this.props;
+                let locationIndex = -1;
+                let locationCode = "";
+                if(regioncode) {
+                    locationIndex = markets.findIndex((market) => { 
+                        return regioncode.toUpperCase() === market.Code.toUpperCase(); 
+                    });
+                    if(locationIndex >= 0) {
+                        locationCode = regioncode.toUpperCase();
+                    }
+                } else {
+                    locationIndex = markets.findIndex((market) => { 
+                        return (market.Properties.findIndex((property) => { 
+                            return propcode.toUpperCase() === property.Code.toUpperCase(); 
+                        }) >= 0);
+                    });
+                    if(locationIndex >= 0) {
+                        locationCode = propcode.toUpperCase();
+                    }
+                }
+                selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
+                    filterType: "location",
+                    filterValue: locationCode 
+                });
             }
-            this.sortOffers(sortValue);
-        }
-
-        this.clearFilter = () => {
-            this.applyDefaultFilterAndSort(true);
-        }
-
-        this.createDefaultFilterAndSort = (searchParams) => {
-            selectedOfferFilters = [];
-            let filterStartDate = getMoment();
-            let filterEndDate = getMoment().add(1, 'month');
-            selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
-                filterType: "date",
-                filterValue: {
-                    startDate: filterStartDate,
-                    endDate: filterEndDate
-                }
-            });
-            if(searchParams) {
-                const {regioncode, propcode, startdate, enddate, flexiblemonth, type, offercode, defaultsort} = searchParams;
-                if(flexiblemonth) {
-                    const monthMoment = getMoment().month(flexiblemonth);
-                    if(monthMoment.isValid()) {
-                        if(monthMoment.month() === getMoment().month()) {
-                            filterStartDate = getMoment();
-                            filterEndDate = getMoment().endOf('month');
-                        } else if(monthMoment.isBefore(getMoment())) {
-                            filterStartDate = monthMoment.clone().add('years', 1).startOf('month');
-                            filterEndDate = monthMoment.clone().add('years', 1).endOf('month');
-                        } else {
-                            filterStartDate = monthMoment.clone().startOf('month');
-                            filterEndDate = monthMoment.clone().endOf('month');
-                        }
-                    }
-                    selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
-                        filterType: "month",
-                        filterValue: {
-                            startDate: filterStartDate,
-                            endDate: filterEndDate
+            if(type) {
+                try {
+                    const offerTypes = getOfferFilterTypes();
+                    const offerTypeFilter = JSON.parse(type.toLowerCase());
+                    const typeFilter = [];
+                    offerTypes.map((type) => {
+                        if(offerTypeFilter.includes(type.toLowerCase())){
+                            typeFilter.push(type);
                         }
                     });
-                } else if(startdate || enddate) {
-                    const urlStartDate = getMoment(startdate);
-                    const urlEndDate = getMoment(enddate);
-                    if(urlStartDate.isValid() && urlStartDate.isSameOrAfter(getMoment(), 'day')) {
-                        filterStartDate = urlStartDate;
-                    }
-                    if(urlEndDate.isValid() && urlEndDate.isSameOrAfter(urlStartDate, 'day')) {
-                        filterEndDate = urlEndDate;
-                    } else {
-                        filterEndDate = filterStartDate.clone().add(1, 'month');
-                    }
                     selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
-                        filterType: "date",
-                        filterValue: {
-                            startDate: filterStartDate,
-                            endDate: filterEndDate
-                        }
+                        filterType: "type",
+                        filterValue: typeFilter
                     });
-                }
-                if(propcode || regioncode) {
-                    const {markets} = this.props;
-                    let locationIndex = -1;
-                    let locationCode = "";
-                    if(regioncode) {
-                        locationIndex = markets.findIndex((market) => { 
-                            return regioncode.toUpperCase() === market.Code.toUpperCase(); 
-                        });
-                        if(locationIndex >= 0) {
-                            locationCode = regioncode.toUpperCase();
-                        }
-                    } else {
-                        locationIndex = markets.findIndex((market) => { 
-                            return (market.Properties.findIndex((property) => { 
-                                return propcode.toUpperCase() === property.Code.toUpperCase(); 
-                            }) >= 0);
-                        });
-                        if(locationIndex >= 0) {
-                            locationCode = propcode.toUpperCase();
-                        }
-                    }
-                    selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
-                        filterType: "location",
-                        filterValue: locationCode 
-                    });
-                }
-                if(type) {
-                    try {
-                        const offerTypes = getOfferFilterTypes();
-                        const offerTypeFilter = JSON.parse(type.toLowerCase());
-                        const typeFilter = [];
-                        offerTypes.map((type) => {
-                            if(offerTypeFilter.includes(type.toLowerCase())){
-                                typeFilter.push(type);
-                            }
-                        });
-                        selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
-                            filterType: "type",
-                            filterValue: typeFilter
-                        });
-                    } catch (ex) {}
-                }
-                if(offercode) {
-                    selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
-                        filterType: "code",
-                        filterValue: offercode.toUpperCase()
-                    });
-                }
-                if(defaultsort) {
-                    selectedOfferSort = defaultsort;
-                }
+                } catch (ex) {}
+            }
+            if(offercode) {
+                selectedOfferFilters = updateSelectedFilter(selectedOfferFilters, {
+                    filterType: "code",
+                    filterValue: offercode.toUpperCase()
+                });
+            }
+            if(defaultsort) {
+                this.selectedOfferSort = defaultsort;
             }
         }
+        this.selectedOfferFilters = selectedOfferFilters;
+    }
 
-        this.applyDefaultFilterAndSort = (isAllFiltersCleared) => {
-            const {location} = this.props;
-            const searchParams = getUrlParams(location ? location.search : "");
-            this.createDefaultFilterAndSort(isAllFiltersCleared ? "" : searchParams);
-            this.updateOfferList();
-        }
+    applyDefaultFilterAndSort(isAllFiltersCleared) {
+        const {location} = this.props;
+        const searchParams = getUrlParams(location ? location.search : "");
+        this.createDefaultFilterAndSort(isAllFiltersCleared ? "" : searchParams);
+        this.updateOfferList();
     }
     
     render() {
