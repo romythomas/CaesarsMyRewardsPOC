3d7b56e78dbd93bceef216f350d71ee8f295659a
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-17 14:54:21 -0400

    Added prod build to Repo

---
+++ dist/precache-manifest.7f8bd12c0bcd3a0d2f4a6295d6b16a7a.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/029b9bc9d72f4d799803735f6295f763.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/043771c999cd4d47d9f33c9c7e42c392.ttf"
+  },
+  {
+    "revision": "80393710ad6954d0e378173b89df3ea0",
+    "url": "/06b9e3d89218b9dc5069b52309b27d36.woff"
+  },
+  {
+    "revision": "009bfe3aad329aef58e2a5588f103e07",
+    "url": "/07073838959a354d99c1fb69dc6e7922.woff"
+  },
+  {
+    "revision": "184c553c59496c96a2d7acca680c85fa",
+    "url": "/12ba843c75312dd308b0c43a97349566.woff"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/17e98b9e5586529b13ccb1f7af16861c.ttf"
+  },
+  {
+    "revision": "a20e37236f886a8f99841c7cde35cbbc",
+    "url": "/1be59373702bc00dd9d5d8f569407668.woff"
+  },
+  {
+    "revision": "78b08a68d05d5fabb0b8effd51bf6ade",
+    "url": "/1c9d53ac65b20d03c4d08ff2cf922862.ttf"
+  },
+  {
+    "revision": "ab459481bb83f2cf53e5da9d7067323b",
+    "url": "/2ca169c90e53d1d0480b5680bfca1d23.woff"
+  },
+  {
+    "revision": "aed326a49d7a5cacf9d309de79c957e4",
+    "url": "/2fb0d38ac7971270e3020bda35b205b3.woff"
+  },
+  {
+    "revision": "05bfb5d918f12e07d0aaebc70f306a82",
+    "url": "/3cd505e52cdc5357abd81b4213f2d8b9.woff"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/3d5923f94fc319b5652a7feb9dfa2b28.ttf"
+  },
+  {
+    "revision": "5bcf9e364ca675da640f1e94c4bee808",
+    "url": "/64bdbbf8465425d4bf819e26f6d671c5.woff"
+  },
+  {
+    "revision": "82763876fe3e7c9954ac3ab7b4138ba2",
+    "url": "/654b3d7eb08497b3a6ac65090bacf195.woff"
+  },
+  {
+    "revision": "fa62ed86e1cd572a3fa996a0a8f09543",
+    "url": "/80708ae2e8dfa6899a5f44497686d9a6.woff"
+  },
+  {
+    "revision": "f77df8728ad348d3c23e63c47d895c67",
+    "url": "/83bfa37caed10df34bc249cc983b08c1.woff"
+  },
+  {
+    "revision": "3b219d6fbc42da21cab83e1fc2fb215e",
+    "url": "/98177d163f74efb869322b05fca4997b.woff"
+  },
+  {
+    "revision": "0949441c78b485adc1dc",
+    "url": "/LeaderBoard.chunk.js"
+  },
+  {
+    "revision": "d3e02f47d5ad750c030e",
+    "url": "/MyOffers.chunk.js"
+  },
+  {
+    "revision": "ebf7642ccfebe47127b7",
+    "url": "/MyRewards.chunk.js"
+  },
+  {
+    "revision": "7845e3df5581e1b28380",
+    "url": "/OfferDetails.chunk.js"
+  },
+  {
+    "revision": "3ea891ad0e06646cff9f",
+    "url": "/QuestForRewards.chunk.js"
+  },
+  {
+    "revision": "e9bb90427783ebc74d03",
+    "url": "/Reservation.chunk.js"
+  },
+  {
+    "revision": "dee93d49769e08ebd68e",
+    "url": "/bundle.js"
+  },
+  {
+    "revision": "798e963a269e84c8d39ec63f1965734d",
+    "url": "/c3c6001eee0c75a7dcc2cb46beedc9db.woff"
+  },
+  {
+    "revision": "b0a6a8a1c6ce3e45f40d42eac2b33b57",
+    "url": "/cdbd86597a115565528e7520fcd68906.woff"
+  },
+  {
+    "revision": "bc62a80cc62cf21407b9d71a116dd67a",
+    "url": "/cdffefafe4a4c52c5dea60f91914582e.woff"
+  },
+  {
+    "revision": "0b8b0941d8442282a7f5ebcd08ae86d2",
+    "url": "/df75a5aea067f7b1d01413be9061482a.woff"
+  },
+  {
+    "revision": "ef5b172f3b618f2fd31c70a623a1f824",
+    "url": "/e5b5315aeec8c6bc62c9ca7ba81af487.woff"
+  },
+  {
+    "revision": "4850785ff4b5536a80f0e2bd977ea7d6",
+    "url": "/ff93faa1ec704c321dea8155fb2e498a.woff"
+  },
+  {
+    "url": "/icon_128x128.9b5167d60bc3e0257240ff24024bbfba.png"
+  },
+  {
+    "url": "/icon_144x144.d370d128d4d996638992022b763342c1.png"
+  },
+  {
+    "url": "/icon_152x152.19374ab632d44b2a92ac2276275a9d48.png"
+  },
+  {
+    "url": "/icon_192x192.a24be1c792d874c37b57ab310b26c554.png"
+  },
+  {
+    "url": "/icon_384x384.3fda4264a9296c1a30b05e69b00e5d96.png"
+  },
+  {
+    "url": "/icon_512x512.bc015ba8aca832d1a087a471f88a8230.png"
+  },
+  {
+    "url": "/icon_72x72.f0e97fcbd66104f85fbf8ba59a7dec8d.png"
+  },
+  {
+    "url": "/icon_96x96.a6419fdf1304f08e159fabb12f788297.png"
+  },
+  {
+    "revision": "c87c60538dd34066f110b4343983b4b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e85b3eba9d6be416c66eb41694a34594",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4de73cb5f5153185614d",
+    "url": "/vendors~MyOffers.chunk.js"
+  },
+  {
+    "revision": "f4151aea4bc0aff2f4fd",
+    "url": "/vendors~MyOffers~MyRewards~OfferDetails.chunk.js"
+  }
+]);
---
+++ dist/src.sw.js
@@ -0,0 +1,35 @@
+importScripts("/precache-manifest.18cebb9b6f69d73a49b47ae63086175b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"
+);
+console.log("Service worker Loaded ");
+//workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+self.addEventListener("push", (e) => {
+  const data = e.data.json();
+  console.log("Push Received ");
+  console.log(data);
+  const { title, id, status, start, end } = data.data;
+  const notificationTitle = "New Offer : " + id + " Title : " + title;
+  const body =
+    "Start Date : " + formatDate(start) + " End Date : " + formatDate(end);
+
+  e.waitUntil(
+    self.registration.showNotification(notificationTitle, {
+      body: body
+    })
+  );
+});
+
+const formatDate = (dateToFormat) => {
+  try {
+    let formattedDate = new Date(dateToFormat);
+    formattedDate = moment(formattedDate).format("MMM-DD-YYYY");
+    return formattedDate;
+  } catch (e) {
+    console.log("Error in date conversion is " + e);
+    throw e;
+  }
+};
+
