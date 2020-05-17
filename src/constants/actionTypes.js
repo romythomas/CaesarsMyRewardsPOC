1e0516959c98a170d39da47c6f17c1089f763ba0
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-17 10:46:21 -0400

    Formatting file

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,5 @@
-export const LOGIN = 'LOGIN';
-export const GET_PROFILE = 'GET_PROFILE';
-export const FILTER_SORT_OFFER = 'FILTER_OFFER';
-export const ASYNC_START = 'ASYNC_START';
-export const ASYNC_END = 'ASYNC_END';
+export const LOGIN = "LOGIN";
+export const GET_PROFILE = "GET_PROFILE";
+export const FILTER_SORT_OFFER = "FILTER_OFFER";
+export const ASYNC_START = "ASYNC_START";
+export const ASYNC_END = "ASYNC_END";
