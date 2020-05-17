7a1d12fe7c78a3b7f5c98e67a5f040585e8767b9
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-17 02:02:01 -0400

    Formatting and restructuring of my rewards components

--- src/components/MyRewards/OfferItem.js
+++ src/components/MyRewards/OfferItem.js
@@ -1,43 +1,47 @@
-import React from 'react';
-import {getProperty,truncate} from '../../utilities/Helper'
-import {getImageUrl} from '../../constants/configs'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { getProperty, truncate } from "../../utilities/Helper";
+import { getImageUrl, getCaesarsDomain } from "../../constants/configs";
+import { Link } from "react-router-dom";
 
-/**
- * 
- * @param {*} props 
- */
-const OfferItem = (props) => { 
-    const { offerList, propertyList} = props;
+const OfferItem = (props) => {
+    const { offerList, propertyList } = props;
+    const offer = offerList && offerList.length ? offerList[0] : null;
+
     let imageUrl = getImageUrl();
-    let propertyName = '';
-    let offers = (offerList[0]== null)? '' : offerList[0];
-    if(propertyList && offers && offers && offers.propertyList[0]){
-        var property = getProperty(propertyList, offers.propertyList[0]);
-        if(property){
-            imageUrl = `http://caesars.com${property.thumbnail.url}/hd/m/cover`;
+    let propertyName = "";
+
+    if (offer && offer.propertyList && offer.propertyList.length && propertyList && propertyList.length) {
+        const { title, description, id } = offer;
+        const offerProperty = offer.propertyList[0];
+        var property = getProperty(propertyList, offerProperty);
+        if (property) {
+            imageUrl = `${getCaesarsDomain()}${property.thumbnail.url}/hd/m/cover`;
             propertyName = property.propertyName;
         }
+
+        return (
+            <div className="listing-wrap">
+                <h3>My Offer</h3>
+                <div className="listing__img">
+                    <img className="thumb" src={imageUrl} alt="offer image" />
+                    <div className="img-info">
+                        <h5>{title}</h5>
+                        <span className="place">{propertyName}</span>
+                    </div>
+                </div>
+                <div className="listing__details">
+                    <h2>{truncate(description, 63)}</h2>
+                </div>
+                <div className="btn-wrap-double">
+                    <Link className="button" id="viewoffer" to={`/offerdetails/${id}`}>
+                        View Offer
+                    </Link>
+                    <Link className="button button-outline" id="viewall" to={`/myoffers`}>
+                        View All
+                    </Link>
+                </div>
+            </div>
+        );
     }
-    return (   
-        <div className="listing-wrap">
-            <h3>My Offer</h3>
-            <div className="listing__img">
-                  <img className="thumb" src={imageUrl} alt="offer image"/>
-                  <div className="img-info">
-                    <h5>{offers.title}</h5>
-                    <span className="place">{propertyName}</span>
-                  </div>
-            </div>
-            <div className="listing__details">
-                <h2>{truncate(offers.description, 63)}</h2>
-                <span className="rate">&nbsp;</span>                
-            </div>
-            <div className="btn-wrap-double">   
-                <Link className="button" id="viewoffer" to={`/offerdetails/${offers.id}`}> View Offer</Link>   
-                <Link className="button button-outline" id="viewall" to={`/myoffers`}> View All</Link>  
-            </div>
-        </div>
-    );
-}
-export default (OfferItem);
+};
+export default OfferItem;
--- src/components/MyRewards/index.js
+++ src/components/MyRewards/index.js
@@ -14,118 +14,101 @@ import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
 import { scrollPageToBanner } from "../../utilities/Helper";
 
 const mapStateToProps = (state) => ({
-  appName: state.common.appName,
-  token: state.common.token,
-  logininfo: state.guestProfile.logininfo,
-  feeds: state.guestProfile.feeds,
-  offers: state.common.offers,
-  markets: state.common.markets,
-  properties: state.common.properties,
-  reservations: state.common.reservations,
-  enterpriseFeed: state.common.enterpriseFeed,
-  priceAlert: state.common.priceAlert
+    appName: state.common.appName,
+    token: state.common.token,
+    logininfo: state.guestProfile.logininfo,
+    feeds: state.guestProfile.feeds,
+    offers: state.common.offers,
+    markets: state.common.markets,
+    properties: state.common.properties,
+    reservations: state.common.reservations,
+    enterpriseFeed: state.common.enterpriseFeed,
+    priceAlert: state.common.priceAlert
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  onGetGuestProfile: (payload) => dispatch({ type: GET_PROFILE, payload })
+    onGetGuestProfile: (payload) => dispatch({ type: GET_PROFILE, payload })
 });
 
 class MyRewards extends Component {
-  componentWillMount() {
-    this.props.onGetGuestProfile(
-      Promise.all([
-        agent.Profile.getGuestProfile(this.props.accountID),
-        agent.Profile.getFeeds()
-      ])
-    );
-  }
-
-  componentDidMount() {
-    scrollPageToBanner();
-  }
-
-  render() {
-    const {
-      logininfo,
-      feeds,
-      properties,
-      offers,
-      enterpriseFeed,
-      priceAlert,
-      reservations
-    } = this.props;
-    if (logininfo && feeds && properties) {
-      //DataLayer
-      try {
-        recordMyRewardsData(
-          "MyRewards",
-          logininfo.tier.tierscore,
-          logininfo.propcode,
-          logininfo.email,
-          logininfo.tier.code,
-          logininfo.accountid
+    componentWillMount() {
+        this.props.onGetGuestProfile(
+            Promise.all([agent.Profile.getGuestProfile(this.props.accountID), agent.Profile.getFeeds()])
         );
-      } catch (err) {
-        //ignore datalayer error
-      }
-
-      return (
-        <div className="container-fluid">
-          <ScrollUpButton
-            ContainerClassName='scroll-top'
-            ShowAtPosition={500}
-          />
-          <div className="title">
-            <h1>My Rewards</h1>
-          </div>
-          <div className="content-box">
-            <div className="row">
-              <div className="col-md-4">
-                <RewardCreditItem logininfo={logininfo} />
-              </div>
-              <div className="col-md-4">
-                <TierScoreItem logininfo={logininfo} />
-              </div>
-              <div className="col-md-4">
-                <TierScoreDetailsItem feeds={feeds} logininfo={logininfo} />
-              </div>
-            </div>
-          </div>
-
-          <ProgressBarItem feeds={feeds} logininfo={logininfo} />
-
-          <div className="listing listing--reward">
-            <ul className="row">
-              <li className="col-md-4 col-sm-6">
-                <OfferItem offerList={offers} propertyList={properties} />
-              </li>
-              <li className="col-md-4 col-sm-6">
-                <ReservationItem
-                  propertyList={properties}
-                  reservationList={reservations}
-                />
-              </li>
-              <li className="col-md-4 col-sm-6">
-                <PriceAlertItem
-                  propertyList={properties}
-                  enterpriseList={enterpriseFeed}
-                  priceList={priceAlert}
-                />
-              </li>
-            </ul>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="container-fluid">
-          <div className="title">
-            <h1>Loading</h1>
-          </div>
-        </div>
-      );
     }
-  }
+
+    componentDidMount() {
+        scrollPageToBanner();
+    }
+
+    render() {
+        const { logininfo, feeds, properties, offers, enterpriseFeed, priceAlert, reservations } = this.props;
+        if (logininfo && feeds && properties) {
+            //DataLayer
+            try {
+                recordMyRewardsData(
+                    "MyRewards",
+                    logininfo.tier.tierscore,
+                    logininfo.propcode,
+                    logininfo.email,
+                    logininfo.tier.code,
+                    logininfo.accountid
+                );
+            } catch (err) {
+                //ignore datalayer error
+            }
+
+            return (
+                <div className="container-fluid">
+                    <ScrollUpButton ContainerClassName="scroll-top" ShowAtPosition={500} />
+                    <div className="title">
+                        <h1>My Rewards</h1>
+                    </div>
+                    <div className="content-box">
+                        <div className="row">
+                            <div className="col-md-4">
+                                <RewardCreditItem logininfo={logininfo} />
+                            </div>
+                            <div className="col-md-4">
+                                <TierScoreItem logininfo={logininfo} />
+                            </div>
+                            <div className="col-md-4">
+                                <TierScoreDetailsItem feeds={feeds} logininfo={logininfo} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <ProgressBarItem feeds={feeds} logininfo={logininfo} />
+
+                    <div className="listing listing--reward">
+                        <ul className="row">
+                            <li className="col-md-4 col-sm-6">
+                                <OfferItem offerList={offers} propertyList={properties} />
+                            </li>
+                            <li className="col-md-4 col-sm-6">
+                                <ReservationItem propertyList={properties} reservationList={reservations} />
+                            </li>
+                            <li className="col-md-4 col-sm-6">
+                                <PriceAlertItem
+                                    propertyList={properties}
+                                    enterpriseList={enterpriseFeed}
+                                    priceList={priceAlert}
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="container-fluid">
+                    <div className="title">
+                        <h1>Loading</h1>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyRewards);
