3ef55737ed15d6be2fb7f1fa33e940400e325cc9
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-17 01:40:33 -0400

    Changed structure of Offer details component and removed unwanted scripts

--- src/components/MyOffers/OfferDetails.js
+++ src/components/MyOffers/OfferDetails.js
@@ -1,236 +1,209 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
-  getProperty,
-  getMoment,
-  getFavouriteClassName,
-  getMarketCodeListOfPropertyCodes,
-  getStructuredMarketsPropertiesList,
-  scrollPageToBanner
+    getProperty,
+    getMoment,
+    getFavouriteClassName,
+    getMarketCodeListOfPropertyCodes,
+    getStructuredMarketsPropertiesList,
+    scrollPageToBanner
 } from "../../utilities/Helper";
 import Autocomplete from "../Common/Autocomplete";
-import { getImageUrl } from "../../constants/configs";
-import {recordOffersDetailsData} from '../../utilities/Gtm-Module';
+import { getImageUrl, getCaesarsDomain } from "../../constants/configs";
+import { recordOffersDetailsData } from "../../utilities/Gtm-Module";
 import ErrorMessage from "../Common/ErrorMessage";
 
 const mapStateToProps = (state) => ({
-  offers: state.common.offers,
-  properties: state.common.properties,
-  markets: state.common.markets
+    offers: state.common.offers,
+    properties: state.common.properties,
+    markets: state.common.markets
 });
 
-const getSelectedPropertyCode = () => {
-  let selectedPropertyCode = "";
-  try {
-    const autocomplete_component = document.getElementById(
-      "autocomplete-component"
-    );
-    const selectedElements = autocomplete_component.querySelectorAll(
-      'li.autocomplete__item[data-selected="true"]'
-    );
-    selectedPropertyCode = selectedElements[0].dataset.value;
-  } catch (ex) {}
-  return selectedPropertyCode;
-};
-const gotoNBE = (parameter) => (event) => {
-  const propCode = getSelectedPropertyCode();
-  if (propCode && parameter && parameter.length) {
-    const url = `http://www.caesars.com/book/?view=ratecal&offerCode=${parameter[0]}&arrival=${parameter[1]}&departure=${parameter[2]}&propCode=${propCode}`;
-    window.location = url;
-  }
-};
-
-const loadScript = () => {
-  $(document).ready(function () {
-    // properties show hide
-    $(".properties-wrap a").click(function (event) {
-      event.preventDefault();
-      event.stopPropagation();
-      $(".properties-list").toggle();
-    });
-
-    $(".properties-list").click(function (event) {
-      event.stopPropagation();
-    });
-    $("body").click(function () {
-      $(".properties-list").hide();
-    });
-  });
-};
-/**
- *
- */
 class OfferDetails extends Component {
-  componentWillMount() {
-    //DataLayer
-    try {
-      recordOffersDetailsData("OfferDetails", id);
-    } catch (err) {
-      //ignore datalayer error
+    constructor(props) {
+        super(props);
+
+        this.offer = "";
+        this.imageUrl = getImageUrl();
+        this.marketPropertyListData = [];
+        this.selectedProperty = "";
+
+        this.onLocationChange = this.onLocationChange.bind(this);
+        this.gotoBookingWebsite = this.gotoBookingWebsite.bind(this);
     }
-  }
-  componentDidMount() {
-    scrollPageToBanner();
-  }
-  render() {
-    loadScript();
-    const { offers, properties, markets, match } = this.props;
-    if (match.params && match.params.id) {
-      const offer = offers.filter((offer) => {
-        return offer.id === this.props.match.params.id;
-      });
-      if (offer && offer.length) {
-        this.offer = offer[0];
-        const {
-          propertyList,
-          id,
-          title,
-          start,
-          end,
-          description,
-          pref
-        } = this.offer;
 
-        let imageUrl = getImageUrl();
-        const property = getProperty(properties, propertyList[0]);
-        if (property) {
-          imageUrl = `http://caesars.com${property.thumbnail.url}`;
-        }
-
-        const marketsOfProperties = getMarketCodeListOfPropertyCodes(
-          markets,
-          propertyList
-        );
-
-        let marketPropertyListData = getStructuredMarketsPropertiesList(
-          markets
-        );
-        marketPropertyListData = marketPropertyListData.filter((data) => {
-          return (
-            marketsOfProperties.includes(data.value) ||
-            propertyList.includes(data.value)
-          );
-        });
-
-        let defaultSelectedProperty = "";
-        const firstProperty = marketPropertyListData.find((item) => {
-          return !item.isMarket;
-        });
-        if (firstProperty && firstProperty.value) {
-          defaultSelectedProperty = firstProperty.value;
-        }
-        return (
-          <div className="container-fluid">
-            <div className="title">
-              <h1>My Offer Details</h1>
-            </div>
-            <div className="offer-details content-box">
-              <div className="row">
-                <div className="col-md-4 col-sm-6">
-                  <div className="thumb">
-                    <img src={imageUrl} alt="offer details image" />
-                    <div className={`fav ${getFavouriteClassName(pref)}`}></div>
-                  </div>
-                </div>
-                <div className="col-md-5 col-sm-6">
-                  <div className="details-text">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <div className="col-md-3 col-sm-12 pull-right">
-                  <div className="details-info">
-                    <span className="offer-code">
-                      OFFER CODE: <strong>{id}</strong>
-                    </span>
-                    <span className="expires">
-                      EXPIRES:{" "}
-                      <strong>{getMoment(end).format("MM/DD/YYYY")}</strong>
-                    </span>
-                  </div>
-                </div>
-                <div className="col-sm-6 col-xs-12">
-                  <div className="details-propertyselect">
-                    <Autocomplete
-                      dataList={marketPropertyListData}
-                      stylingClass={"disabled"}
-                      defaultValue={defaultSelectedProperty}
-                      elementId="navigate-from-offer-details"
-                      title="Where do you want to go?"
-                    />
-                  </div>
-                </div>
-                <div className="col-sm-6 col-xs-12">
-                  <button
-                    className="button"
-                    id="book"
-                    onClick={gotoNBE(
-                      new Array(
-                        id,
-                        getMoment(start).format("MM/DD/YYYY"),
-                        getMoment(end).format("MM/DD/YYYY")
-                      )
-                    )}
-                  >
-                    Book
-                  </button>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-12 col-sm-12">
-                  <div className="how-redeem">
-                    <strong>
-                      <h5>HOW TO REDEEM</h5>
-                    </strong>
-                    <div className="redeem-content">
-                      <p>
-                        To redeem your offer, please follow the instructions on
-                        your mail piece or email. A print out of this page is
-                        NOT a physical coupon and cannot be used for redemption
-                        where a coupon is required and some offers require a
-                        physical coupon to redeem. If this is a hotel offer and
-                        you book it online; you do not need to bring in your
-                        physical coupon when you check in. It is always best to
-                        bring your invite or mail piece with you when you visit.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-12 col-sm-12">
-                  <div className="terms">
-                    <strong>
-                      <h5>TERMS &amp; CONDITIONS</h5>
-                    </strong>
-                    <div className="terms-content">
-                      <p>
-                        Caesars Rewards offers are non-transferable and
-                        non-negotiable. Offer is only valid at specified
-                        casinos. Caesars Rewards Card and valid photo ID must be
-                        presented upon redemption. Not responsible for lost or
-                        stolen vouchers or coupons. Offers are based upon
-                        availability. Effective July 1, 2017, complimentary
-                        rooms booked will be subject to a $50 no-show fee, plus
-                        tax if the reservation is not canceled by 6pm on the day
-                        of arrival. Excludes bookings made for Caesars Windsor.
-                        Additional restrictions may apply. Please see your
-                        nearest Caesars Rewards Center for more details.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      }
+    componentWillMount() {
+        this.findOfferDetails();
+        this.updateImageUrl();
+        this.updateMarketPropertyValues();
     }
-    return (
-      <ErrorMessage 
-        errorText="No offer details available." />
-    );
-  }
+
+    componentDidMount() {
+        scrollPageToBanner();
+        //DataLayer
+        try {
+            recordOffersDetailsData("OfferDetails", id);
+        } catch (err) {
+            //ignore datalayer error
+        }
+    }
+
+    findOfferDetails() {
+        const { offers, match } = this.props;
+        if (offers && offers.length && match.params && match.params.id) {
+            const offerItem = offers.filter((offer) => {
+                return offer.id.toLowerCase() === match.params.id.toLowerCase();
+            });
+            if (offerItem && offerItem.length) {
+                this.offer = offerItem[0];
+            }
+        }
+    }
+
+    updateImageUrl() {
+        const { properties } = this.props;
+        const { propertyList } = this.offer;
+        if (properties && properties.length && propertyList && propertyList.length) {
+            const property = getProperty(properties, propertyList[0]);
+            if (property) {
+                this.imageUrl = `http://caesars.com${property.thumbnail.url}`;
+            }
+        }
+    }
+
+    updateMarketPropertyValues() {
+        const { markets } = this.props;
+        const { propertyList } = this.offer;
+        if (markets && markets.length && propertyList && propertyList.length) {
+            const marketsOfProperties = getMarketCodeListOfPropertyCodes(markets, propertyList);
+            this.marketPropertyListData = getStructuredMarketsPropertiesList(markets);
+            this.marketPropertyListData = this.marketPropertyListData.filter((data) => {
+                return marketsOfProperties.includes(data.value) || propertyList.includes(data.value);
+            });
+            const firstProperty = this.marketPropertyListData.find((item) => {
+                return !item.isMarket;
+            });
+            if (firstProperty && firstProperty.value) {
+                this.selectedProperty = firstProperty.value;
+            }
+        }
+    }
+
+    onLocationChange(value) {
+        if (value) {
+            this.selectedProperty = value;
+        }
+    }
+
+    gotoBookingWebsite() {
+        const { selectedProperty, offer } = this;
+        if (selectedProperty && offer) {
+            let { id, start, end } = offer;
+            if (id && start && end) {
+                start = getMoment(start).format("MM/DD/YYYY");
+                end = getMoment(end).format("MM/DD/YYYY");
+                const redirectionUrl = `${getCaesarsDomain()}/book/?propCode=${selectedProperty}&view=ratecal&arrival=${start}&departure=${end}&offerCode=${id}`;
+                window.location = redirectionUrl;
+            }
+        }
+    }
+
+    render() {
+        if (this.offer) {
+            const { id, title, end, description, pref } = this.offer;
+
+            return (
+                <div className="container-fluid">
+                    <div className="title">
+                        <h1>My Offer Details</h1>
+                    </div>
+                    <div className="offer-details content-box">
+                        <div className="row">
+                            <div className="col-md-4 col-sm-6">
+                                <div className="thumb">
+                                    <img src={this.imageUrl} alt="offer details image" />
+                                    <div className={`fav ${getFavouriteClassName(pref)}`}></div>
+                                </div>
+                            </div>
+                            <div className="col-md-5 col-sm-6">
+                                <div className="details-text">
+                                    <h2>{title}</h2>
+                                    <p>{description}</p>
+                                </div>
+                            </div>
+                            <div className="col-md-3 col-sm-12 pull-right">
+                                <div className="details-info">
+                                    <span className="offer-code">
+                                        OFFER CODE: <strong>{id}</strong>
+                                    </span>
+                                    <span className="expires">
+                                        EXPIRES: <strong>{getMoment(end).format("MM/DD/YYYY")}</strong>
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="col-sm-6 col-xs-12">
+                                <div className="details-propertyselect">
+                                    <Autocomplete
+                                        dataList={this.marketPropertyListData}
+                                        stylingClass={"disabled"}
+                                        defaultValue={this.selectedProperty}
+                                        elementId="navigate-from-offer-details"
+                                        title="Where do you want to go?"
+                                        onChange={this.onLocationChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="col-sm-6 col-xs-12">
+                                <button className="button" id="book" onClick={this.gotoBookingWebsite}>
+                                    Book
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12 col-sm-12">
+                                <div className="how-redeem">
+                                    <strong>
+                                        <h5>HOW TO REDEEM</h5>
+                                    </strong>
+                                    <div className="redeem-content">
+                                        <p>
+                                            To redeem your offer, please follow the instructions on your mail piece or email. A
+                                            print out of this page is NOT a physical coupon and cannot be used for redemption
+                                            where a coupon is required and some offers require a physical coupon to redeem. If
+                                            this is a hotel offer and you book it online; you do not need to bring in your
+                                            physical coupon when you check in. It is always best to bring your invite or mail
+                                            piece with you when you visit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12 col-sm-12">
+                                <div className="terms">
+                                    <strong>
+                                        <h5>TERMS &amp; CONDITIONS</h5>
+                                    </strong>
+                                    <div className="terms-content">
+                                        <p>
+                                            Caesars Rewards offers are non-transferable and non-negotiable. Offer is only valid at
+                                            specified casinos. Caesars Rewards Card and valid photo ID must be presented upon
+                                            redemption. Not responsible for lost or stolen vouchers or coupons. Offers are based
+                                            upon availability. Effective July 1, 2017, complimentary rooms booked will be subject
+                                            to a $50 no-show fee, plus tax if the reservation is not canceled by 6pm on the day of
+                                            arrival. Excludes bookings made for Caesars Windsor. Additional restrictions may
+                                            apply. Please see your nearest Caesars Rewards Center for more details.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return <ErrorMessage errorText="No offer details available." />;
+    }
 }
 
 export default connect(mapStateToProps)(OfferDetails);
--- src/constants/configs.js
+++ src/constants/configs.js
@@ -3,7 +3,7 @@ const staticFilesDomain = "https://caesarspoc.s3.us-east-2.amazonaws.com";
 
 export const getOfferFilterTypes = () => {
     return ["Hotel", "Cash", "Gaming", "Entertainment", "Events", "Dining", "Other", "Package", "Favorite"];
-}
+};
 
 export const getOfferSortTypes = () => {
     return [
@@ -16,12 +16,16 @@ export const getOfferSortTypes = () => {
             name: "Preference"
         }
     ];
-}
+};
 
-export const getImageUrl =()=>{
-    return caesarsDomain + '/myrewards/profile/images/tr-placeholder.jpg';
-}
+export const getCaesarsDomain = () => {
+    return caesarsDomain;
+};
 
-export const getApiRootUrl =() =>{
+export const getImageUrl = () => {
+    return caesarsDomain + "/myrewards/profile/images/tr-placeholder.jpg";
+};
+
+export const getApiRootUrl = () => {
     return staticFilesDomain + "/stubs/";
-}
+};
