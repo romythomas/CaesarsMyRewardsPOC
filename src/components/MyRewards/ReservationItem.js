207cba52d31347593f2d02bc37aafc03d98fb74c
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-20 13:00:52 -0400

    Fixed date format issue in my rewards page - reservation box

--- src/components/MyRewards/ReservationItem.js
+++ src/components/MyRewards/ReservationItem.js
@@ -1,46 +1,45 @@
-import React from 'react';
-import {getProperty, getMoment} from '../../utilities/Helper'
-import {getImageUrl} from '../../constants/configs'
+import React from "react";
+import { getProperty, getMoment } from "../../utilities/Helper";
+import { getImageUrl } from "../../constants/configs";
 
-  /**
-   * 
-   * @param {*} props 
-   */
-const ReservationItem = (props) => { 
-    const {propertyList, reservationList} = props;
+/**
+ *
+ * @param {*} props
+ */
+const ReservationItem = (props) => {
+    const { propertyList, reservationList } = props;
 
     let imageUrl = getImageUrl();
-    let propertyName = '';
+    let propertyName = "";
     let reservations = reservationList.reservations[0];
-    if(propertyList && reservations && reservations && reservations.propertyCode){
+    if (propertyList && reservations && reservations && reservations.propertyCode) {
         let property = getProperty(props.propertyList, reservations.propertyCode);
-        if(property){
-            imageUrl =`http://caesars.com${property.thumbnail.url}/hd/m/cover`;
+        if (property) {
+            imageUrl = `http://caesars.com${property.thumbnail.url}/hd/m/cover`;
             propertyName = property.propertyName.toUpperCase();
         }
     }
-    
-    return (        
+
+    return (
         <div className="listing-wrap">
             <h3>My Reservation</h3>
             <div className="listing__img">
-                <img className="thumb" src={imageUrl} alt="Caesars"/>
+                <img className="thumb" src={imageUrl} alt="Caesars" />
                 <div className="img-info">
                     <h5>{propertyName}</h5>
-                    <span className="place">&nbsp;</span>
+                    <span className="place"></span>
                 </div>
             </div>
             <div className="listing__details">
-                <h2>Date:- &nbsp;&nbsp;
-                    {getMoment(reservations.checkInDate).format("MM/DD/YYYY")} 
-                    &nbsp;- &nbsp;{getMoment(reservations.checkOutDate).format("MM/DD/YYYY")}</h2>
-                <span className="rate">&nbsp;</span>
+                <h2>Check-in: {getMoment(new Date(reservations.checkInDate)).format("MM/DD/YYYY")}</h2>
+                <h2>Check-out: {getMoment(new Date(reservations.checkOutDate)).format("MM/DD/YYYY")}</h2>
+                <span className="rate"></span>
             </div>
             <div className="btn-wrap-double">
                 <button className="button">View All</button>
             </div>
         </div>
     );
-}
+};
 
-export default (ReservationItem);
+export default ReservationItem;
