0c87d9fa7ae0b999cbf2b36c10e3c4911a37fdda
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-21 23:56:47 -0400

    Update webpack to load fonts in separate folder

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,15 @@ module.exports = {
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ["file-loader"]
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts/"
+                        }
+                    }
+                ]
             }
         ]
     },
