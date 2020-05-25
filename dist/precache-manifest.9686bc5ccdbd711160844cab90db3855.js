77864d83b140baea56af56d176295f408d803ad8
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-24 20:04:13 -0400

    Build movement

--- dist/precache-manifest.9686bc5ccdbd711160844cab90db3855.js
+++ dist/precache-manifest.9686bc5ccdbd711160844cab90db3855.js
@@ -24,40 +24,39 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Reservation.chunk.js"
   },
   {
-    "revision": "baafa092f269e647f6f1",
-    "url": "/bundle.js?v=4"
+    "url": "/bundle.2a12cb88807acf9c9658.js"
   },
   {
     "revision": "798e963a269e84c8d39ec63f1965734d",
-    "url": "/fonts/Montserrat-Black.woff"
+    "url": "/fonts/Montserrat-Black.c3c6001eee0c75a7dcc2cb46beedc9db.woff"
   },
   {
     "revision": "184c553c59496c96a2d7acca680c85fa",
-    "url": "/fonts/Montserrat-Bold.woff"
+    "url": "/fonts/Montserrat-Bold.12ba843c75312dd308b0c43a97349566.woff"
   },
   {
     "revision": "ab459481bb83f2cf53e5da9d7067323b",
-    "url": "/fonts/Montserrat-Regular.woff"
+    "url": "/fonts/Montserrat-Regular.2ca169c90e53d1d0480b5680bfca1d23.woff"
   },
   {
     "revision": "bc62a80cc62cf21407b9d71a116dd67a",
-    "url": "/fonts/Montserrat-SemiBold.woff"
+    "url": "/fonts/Montserrat-SemiBold.cdffefafe4a4c52c5dea60f91914582e.woff"
   },
   {
     "revision": "50145685042b4df07a1fd19957275b81",
-    "url": "/fonts/OpenSans-Bold.ttf"
+    "url": "/fonts/OpenSans-Bold.043771c999cd4d47d9f33c9c7e42c392.ttf"
   },
   {
     "revision": "1bf71be111189e76987a4bb9b3115cb7",
-    "url": "/fonts/OpenSans-Light.ttf"
+    "url": "/fonts/OpenSans-Light.3d5923f94fc319b5652a7feb9dfa2b28.ttf"
   },
   {
     "revision": "629a55a7e793da068dc580d184cc0e31",
-    "url": "/fonts/OpenSans-Regular.ttf"
+    "url": "/fonts/OpenSans-Regular.17e98b9e5586529b13ccb1f7af16861c.ttf"
   },
   {
     "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
-    "url": "/fonts/OpenSans-Semibold.ttf"
+    "url": "/fonts/OpenSans-Semibold.029b9bc9d72f4d799803735f6295f763.ttf"
   },
   {
     "url": "/icon_128x128.9b5167d60bc3e0257240ff24024bbfba.png"
@@ -84,7 +83,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/icon_96x96.a6419fdf1304f08e159fabb12f788297.png"
   },
   {
-    "revision": "3dfd6bc192e21643f7b87f5f0de43ad4",
+    "revision": "761bba5f9b8bb874217172b68db766ce",
     "url": "/index.html"
   },
   {
--- dist/src.sw.js
+++ dist/src.sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.245d7723a3dab2745188e700b7c431f3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.9686bc5ccdbd711160844cab90db3855.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
   "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"
