f19bff8350ddbf19ad965144ee63f9f526302a04
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-25 12:01:25 -0400

    Date issue fix in IOS <Trial fix 3>

--- src/components/MyRewards/ReservationItem.js
+++ src/components/MyRewards/ReservationItem.js
@@ -32,14 +32,8 @@ const ReservationItem = (props) => {
                     </div>
                 </div>
                 <div className="listing__details">
-                    <h2>
-                        Check-in:{" "}
-                        {checkInDate ? getMoment(new Date(checkInDate.toString()), "MM-DD-YYYY").format("MM/DD/YYYY") : ""}
-                    </h2>
-                    <h2>
-                        Check-out:{" "}
-                        {checkOutDate ? getMoment(new Date(checkOutDate.toString()), "MM-DD-YYYY").format("MM/DD/YYYY") : ""}
-                    </h2>
+                    <h2>Check-in: {checkInDate ? checkInDate.toString() : "No Date"}</h2>
+                    <h2>Check-out: {checkOutDate ? checkOutDate.toString() : ""}</h2>
                     <span className="rate">&nbsp;</span>
                 </div>
                 <div className="btn-wrap-double">
